75f4403eef408e456a300037423747845d528437
YorelN <nicolas.leroy@hetic.net>
2018-06-14 11:48:27 +0200

    Finish front-end verification

--- web/js/actions.js
+++ web/js/actions.js
@@ -15,7 +15,6 @@
   for (var i = 0; i < INPUTS.length; i++) {
     const INPUT = INPUTS[i];
     const INPUT_NAME = INPUT.getAttribute("name");
-
     if (!INPUT_NAME) {
       throw new Error(
         "[ WARNING ]: Inputs must provide a name, the " + (i + 1) + " does'nt"
@@ -30,10 +29,24 @@
     });
   }
 
+  FORM.addEventListener("change", function() {
+    if (self.initialState.email && !validateEmail(self.initialState.email)) {
+      throw new Error("[ ERRROR ]: Not a valid email");
+    }
+  });
+
   FORM.addEventListener("submit", function(event) {
     event.preventDefault();
-    // Temporary workaround: Waiting for the API routes name.
+
+    if (
+      !validatePassword(
+        self.initialState.password,
+        self.initialState.confirmPassword
+      )
+    )
+      throw new Error("[ ERROR ]: These two passwords are not the same");
     if (FORM.name && FORM.name === "LOGIN") {
+      // Temporary workaround: Waiting for the API routes name.
       // axios.post('/login', self.initialState)
     }
 
@@ -41,4 +54,13 @@
       // axios.post('/signup', self.initialState)
     }
   });
+
+  function validatePassword(password, confirmPassword) {
+    return password === confirmPassword;
+  }
+
+  function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 })();
