73341204c7b160f36c1a146e5f63d304cbd67c9f
Nicolas LEROY <nicolas.leroy@hetic.net>
2018-06-14 16:40:30 +0200

    Fix cancel action on create user modal (#81)

--- web/js/client.js
+++ web/js/client.js
@@ -1,16 +1,13 @@
 (function() {
+  // Modal opnening
+  let count = 0;
+  const addClientButton = document.querySelector(".addClientButton");
+  const modal = document.querySelector("div.create-client__modal");
+  var layout = document.querySelector(".layout");
+  var cancelBtn = document.querySelector(".cancel");
 
-  
-    // Modal opnening
-    count = 0;
-    const addClientButton = document.querySelector(".addClientButton");
-    const modal = document.querySelector("div.create-client__modal");
-    var layout =  document.querySelector('.layout');
-    var cancelBtn =  document.querySelector('.cancel');
-
-
-    if(addClientButton) {
-      addClientButton.addEventListener("click", () => {
+  if (addClientButton) {
+    addClientButton.addEventListener("click", () => {
       if (count === 3) {
         count = 0;
         for (var i = 0; i < STEPS.length; i++) {
@@ -30,7 +27,9 @@
     const ACTION_BUTTON = document.querySelector(".next-step");
 
     const CURRENT_STEPPER = document.querySelectorAll(".stepper__step");
-    const CURRENT_STEPPER_COUNT = document.querySelector(".stepper__step-number");
+    const CURRENT_STEPPER_COUNT = document.querySelector(
+      ".stepper__step-number"
+    );
 
     const INFOS_ACTION_BUTTON = document.querySelector(
       ".next-step span.btn__inner-text:first-child"
@@ -38,8 +37,11 @@
     const ALL_INPUTS = document.querySelectorAll("input");
 
     ACTION_BUTTON.addEventListener("click", function() {
+      console.log(count, "init");
       if (count !== STEPS.length) {
         count++;
+        console.log(count, "++");
+
         if (CURRENT_STEPPER[count]) {
           CURRENT_STEPPER[count].classList.toggle("current");
         }
@@ -76,28 +78,38 @@
       }
 
       for (var i = 0; i < CURRENT_STEPPER.length; i++) {
-        CURRENT_STEPPER[i].classList.toggle("current");
+        CURRENT_STEPPER[i].classList.remove("current");
+      }
+
+      for (var i = 0; i < STEPS.length; i++) {
+        STEPS[i].classList.remove("create-client__modal-step-active");
       }
     });
   }
 
-
   const targetPopinAccount = document.querySelector(".open-edit");
   const targetPopinProcess = document.querySelector(".open-process");
-  const targetPopinComplementary = document.querySelector(".open-complementary");
+  const targetPopinComplementary = document.querySelector(
+    ".open-complementary"
+  );
 
-  const popinAccount = document.querySelector('.edit__accountInformation');
-  const popinProcess = document.querySelector('.edit__process');
-  const popinComplementary = document.querySelector('.edit__complementaryInfos');
+  const popinAccount = document.querySelector(".edit__accountInformation");
+  const popinProcess = document.querySelector(".edit__process");
+  const popinComplementary = document.querySelector(
+    ".edit__complementaryInfos"
+  );
 
-  const popinAccountCancel = document.querySelector('.edit__accountInformation .cancel');
-  const popinProcessCancel = document.querySelector('.edit__process .cancel');
-  const popinComplementaryCancel = document.querySelector('.edit__complementaryInfos');
-  
-  const layoutDash =  document.querySelector('.layout');
+  const popinAccountCancel = document.querySelector(
+    ".edit__accountInformation .cancel"
+  );
+  const popinProcessCancel = document.querySelector(".edit__process .cancel");
+  const popinComplementaryCancel = document.querySelector(
+    ".edit__complementaryInfos"
+  );
 
-  if(targetPopinAccount && targetPopinProcess && targetPopinComplementary ) {
+  const layoutDash = document.querySelector(".layout");
 
+  if (targetPopinAccount && targetPopinProcess && targetPopinComplementary) {
     targetPopinAccount.addEventListener("click", function() {
       popinAccount.classList.add("open");
       layoutDash.classList.add("layout__dashboard");
@@ -128,5 +140,4 @@
       layoutDash.classList.remove("layout__dashboard");
     });
   }
-
 })();
