06d9d8c9d0fe523cd116ff852ad0f89c8c2be4c0
YorelN <nicolas.leroy@hetic.net>
2018-06-14 11:21:14 +0200

    Add verification to the save data process

--- web/js/actions.js
+++ web/js/actions.js
@@ -6,9 +6,21 @@
   const FORM = document.querySelector("form.formData");
   const INPUTS = document.querySelectorAll("form.formData input");
 
+  if (!FORM.name) {
+    throw new Error(
+      "[ WARNING ]: Form must provide a name to know wich action call"
+    );
+  }
+
   for (var i = 0; i < INPUTS.length; i++) {
     const INPUT = INPUTS[i];
     const INPUT_NAME = INPUT.getAttribute("name");
+
+    if (!INPUT_NAME) {
+      throw new Error(
+        "[ WARNING ]: Inputs must provide a name, the " + (i + 1) + " does'nt"
+      );
+    }
 
     INPUT.addEventListener("keyup", function() {
       self.initialState = {
@@ -20,13 +32,12 @@
 
   FORM.addEventListener("submit", function(event) {
     event.preventDefault();
-
     // Temporary workaround: Waiting for the API routes name.
-    if (FORM.name === "LOGIN") {
+    if (FORM.name && FORM.name === "LOGIN") {
       // axios.post('/login', self.initialState)
     }
 
-    if (FORM.name === "SIGNUP") {
+    if (FORM.name && FORM.name === "SIGNUP") {
       // axios.post('/signup', self.initialState)
     }
   });
